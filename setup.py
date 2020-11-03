26cca07de27d57fb6a3ee0daf1600f764c9159cd
Arpit-Sharma-USC <arpitshrm07@gmail.com>
2020-11-03 22:00:56 +0100

    stage test

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ commands = {
 }
 
 setup(
-    name='bynder-sdk-test-stage',
+    name='bynder-sdk-stage-test',
     version=build_version,
     description=(
         'Bynder SDK can be used to speed up the'
