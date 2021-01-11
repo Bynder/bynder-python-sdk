cf1deeefea7334859e4949d1f37a0a20075c635f
Wouter Toering <wouter.toering@gmail.com>
2021-01-11 14:53:38 +0100

    Combine BynderOAuth2Session and SessionMixin

--- bynder_sdk/oauth2.py
+++ bynder_sdk/oauth2.py
@@ -1,9 +1,20 @@
 import random
 import string
 
+from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
-from bynder_sdk.util import SessionMixin
+from bynder_sdk.version import VERSION
+
+UA_HEADER = {
+    'User-Agent': 'bynder-python-sdk/{}'.format(VERSION)
+}
+
+
+def api_endpoint_url(session, endpoint):
+    if endpoint.startswith('/v6') or endpoint.startswith('/v7'):
+        return 'https://{}{}'.format(session.bynder_domain, endpoint)
+    return 'https://{}/api{}'.format(session.bynder_domain, endpoint)
 
 
 def oauth2_url(bynder_domain, endpoint):
@@ -11,7 +22,7 @@ def oauth2_url(bynder_domain, endpoint):
         bynder_domain, endpoint)
 
 
-class BynderOAuth2Session(SessionMixin, OAuth2Session):
+class BynderOAuth2Session(OAuth2Session):
     def __init__(self, bynder_domain, *args, **kwargs):
         self.bynder_domain = bynder_domain
 
@@ -39,3 +50,32 @@ class BynderOAuth2Session(SessionMixin, OAuth2Session):
             *args,
             **kwargs
         )
+
+    def wrapped_request(self, func, endpoint, *args, **kwargs):
+        endpoint = api_endpoint_url(self, endpoint)
+        response = func(endpoint, *args, **kwargs)
+        response.raise_for_status()
+        try:
+            return response.json()
+        except ValueError:
+            return response
+
+    def get(self, url, *args, **kwargs):
+        return self.wrapped_request(super().get, url, *args, **kwargs)
+
+    def post(self, url, *args, **kwargs):
+        if url.startswith('https'):
+            # Do not send the Authorization header to S3
+            kwargs['headers'] = {'Authorization': None}
+            kwargs['withhold_token'] = True
+            return super().post(url, *args, **kwargs)
+        return self.wrapped_request(super().post, url, *args, **kwargs)
+
+    def put(self, url, *args, **kwargs):
+        return self.wrapped_request(super().put, url, *args, **kwargs)
+
+    def delete(self, url, *args, **kwargs):
+        return self.wrapped_request(super().delete, url, *args, **kwargs)
+
+    def _set_ua_header(self):
+        self.headers.update(UA_HEADER)
